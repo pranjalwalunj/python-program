ad2df01f7f626d368deb60c170cf0d3b6d1dd00e
Pranjal <pranjalwalunj02@gmail.com>
2022-01-06 12:51:19 +0530

    first

---
+++ first.py
@@ -0,0 +1,10 @@
+name = "lucy"
+if name is "pranjal":
+    print('hey there pranjal')
+elif name is "sammy":
+    print('whatup sammy?')
+elif name is "lucy":
+    print('whatup lucy?')
+else:
+    print('please sign up for the site')
+
