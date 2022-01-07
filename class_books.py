d1bc8c2592ae586ba1acc0552307446624ba9e1f
Pranjal <pranjalwalunj02@gmail.com>
2022-01-07 14:22:31 +0530

    Created book as class

--- class_books.py
+++ class_books.py
@@ -7,10 +7,6 @@ class Books(object):
     def book_name(self, name_of_book):
         return f'{name_of_book}'
 
-    def what_i_do(self):
-        sentence = 'i love reading'
-        return sentence
-
     def more_info(self):
         return f'I like {self.author}, I read {self.genre} types of books, I have read {self.name_of_book}'
 
